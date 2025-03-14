d229107eee8fcff699c61873950214da7301b76b
0Armaan025 <armaan33000@gmail.com>
2025-03-14 22:49:32 +0530

    footer fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="relative mt-10 text-center py-4 text-gray-400 text-sm border-t border-gray-600/30 before:absolute before:top-0 before:left-0 before:w-full before:h-[1px] before:bg-gradient-to-r before:from-transparent before:via-indigo-400/50 before:to-transparent">
-        Rinnku &copy; 205 C. All rights reserved.
+        Rinnku &copy; 2025. All rights reserved.
       </footer>
     );
   }
